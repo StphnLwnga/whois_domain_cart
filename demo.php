e38eb02d55ec02b91e0bd63fd532cc5c91c5374c
StphnLwnga <martina@ipayafrica.com>
2019-11-15 12:59:21 +0300

    domain_cart

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php include"cwcconf.php"; ?>
+<html>
+<head>
+<link href="cwhoisstyles.css" rel="stylesheet" type="text/css"> 
+<title>cWhois Domain Cart</title>
+</head>
+<body bgcolor="#FFFFFF">
+<?php include"cwhoiscart.php"; ?>
+</body>
+</html>
